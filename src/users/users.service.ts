da6c1d4aa1abcd788a6fd2e49009dce12b972335
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-09-09 08:48:03 -0300

    create user criado

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Paulo Salvatore',
+      email: 'profpaulo.salvatore@gmail.com',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const id = this.users[this.users.length - 1].id + 1;
+
+    const user: User = {
+      id: id,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+
+    return user;
   }
 
   findAll() {
