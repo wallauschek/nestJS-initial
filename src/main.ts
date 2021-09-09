ed81e0196f3f0c6ffa628c1d26e6f5c63a399847
Pedro Wallauschek <pedro@pedrowallauschek.com.br>
2021-09-09 10:08:56 -0300

    implementando interceptor  e handle error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundInterceptor } from './interceptors/entity-not-found.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Pipes
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -12,6 +14,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  //Interceptor
+  app.useGlobalInterceptors(new EntityNotFoundInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
